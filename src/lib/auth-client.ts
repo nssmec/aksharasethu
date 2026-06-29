d1a361b45a83cb98b0b30a27a8ed20c989cd184c
Hashim <hashimmohammed212@gmail.com>
2026-06-30 01:26:21 +0530

    feat: updated config to remove TS error

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,12 @@
-import { createAuthClient } from "better-auth/react"
+import { createAuthClient } from "better-auth/react";
+import { inferAdditionalFields } from "better-auth/client/plugins";
+import { auth } from "./auth";
 
 export const authClient = createAuthClient({
-    baseURL: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000"
-})
+    baseURL: process.env.NEXT_PUBLIC_APP_URL ?? "http://localhost:3000",
+    plugins: [
+        inferAdditionalFields<typeof auth>(),
+    ],
+});
 
-export const { useSession, signIn, signOut } = authClient
+export const { useSession, signIn, signOut } = authClient;
